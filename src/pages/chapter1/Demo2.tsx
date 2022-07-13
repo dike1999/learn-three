9126bb68bc275e8432feebd9ef4cd1102acab8d9
dike <dike@kuaishou.com>
2022-07-13 19:35:04 +0800

    update: demo2

--- src/pages/chapter1/Demo2.tsx
+++ src/pages/chapter1/Demo2.tsx
@@ -4,12 +4,8 @@ import * as THREE from "three";
 const Demo2 = () => {
   const demo = useRef<HTMLDivElement>(null);
 
-  const init = () => {
-    const scene = new THREE.Scene();
-
-    const axes = new THREE.AxesHelper(20);
-    scene.add(axes);
-
+  // 平面+坐标轴
+  const createPlaneAxes = (scene: THREE.Scene) => {
     const planeGeometry = new THREE.PlaneGeometry(60, 20);
     const planeMaterial = new THREE.MeshBasicMaterial({
       color: 0xaaaaaa,
@@ -18,7 +14,11 @@ const Demo2 = () => {
     plane.rotation.x = -0.5 * Math.PI;
     plane.position.set(15, 0, 0);
     scene.add(plane);
-
+    const axes = new THREE.AxesHelper(50);
+    scene.add(axes);
+  };
+  // 正方体
+  const createCube = (scene: THREE.Scene) => {
     const cubeGeometry = new THREE.BoxGeometry(4, 4, 4);
     const cubeMaterial = new THREE.MeshBasicMaterial({
       color: 0xff0000,
@@ -27,7 +27,9 @@ const Demo2 = () => {
     const cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
     cube.position.set(-4, 3, 0);
     scene.add(cube);
-
+  };
+  // 球
+  const createSphere = (scene: THREE.Scene) => {
     const sphereGeometry = new THREE.SphereGeometry(4, 20, 20);
     const sphereMaterial = new THREE.MeshBasicMaterial({
       color: 0x7777ff,
@@ -36,6 +38,14 @@ const Demo2 = () => {
     const sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
     sphere.position.set(20, 4, 2);
     scene.add(sphere);
+  };
+
+  const init = () => {
+    const scene = new THREE.Scene();
+
+    createPlaneAxes(scene);
+    createCube(scene);
+    createSphere(scene);
 
     const camera = new THREE.PerspectiveCamera(
       45,
